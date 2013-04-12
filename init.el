588a40ab96e060e55032f753be9cda9a2484cdfa
Zak King <zaking17@gmail.com>
2013-04-12 13:51:12 -0700

    ediff setup as default git diff/merge tool

--- init.el
+++ init.el
@@ -65,6 +65,27 @@
 (setq clean-buffer-list-deelay-general 0)
 (setq magic-mode-alist ())
 
+;; http://whattheemacsd.com/file-defuns.el-01.html
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+;; start emacsserver
+(server-start)
+
 ;;-----------------------------------------------------------------------
 ;; movement
 ;;-----------------------------------------------------------------------
