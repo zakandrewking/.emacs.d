67820c0dfd2a99e14996aaff6a690839c14d2ad8
Zak King <zaking17@gmail.com>
2013-12-04 10:40:48 -0800

    etags script can now ignore directories

--- etags.sh
+++ etags.sh
@@ -4,14 +4,17 @@
 # Place the following in file called post-checkout in .git/hooks/
 #
 #     #!/bin/bash
-#     /Users/zaking/.emacs.d/etags.sh -py -m -js
+#     /Users/zaking/.emacs.d/etags.sh -py -m -js -!/env/
+#
+# Ignores the /env/ directory anywhere in the path.
 
 from sys import argv
 from subprocess import call
 
-types = "|".join([a.strip('-') for a in argv[1:]])
-    
-cmd = 'find -E . -regex ".*\.(%s)$" -exec etags -a {} \;' % types
+types = "|".join([a.strip('-') for a in argv[1:] if not "!" in a])
+ignore = " ".join(['\! -path "*%s*"' % a.strip('-!')  for a in argv[1:] if "!" in a])
+
+cmd = 'rm TAGS;find -E . %s \! -path "*/.*" -regex ".*\.(%s)$" -exec etags -a {} \;' % (ignore, types)
 
 status = call(cmd, shell=True)
 print "generated TAGS file"
