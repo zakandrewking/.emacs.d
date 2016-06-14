564f275fb3dd464089bba5dd54d2a6af69633dca
Zachary King <zaking17@gmail.com>
2016-06-13 20:32:02 -0700

    js2-mode patch for object spread

--- js2-setup.el
+++ js2-setup.el
@@ -1,3 +1,6 @@
+(add-to-list 'load-path "~/.emacs.d/js2-patch/")
+(require 'js2-mode)
+
 ;; js2-mode setup
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\'" . javascript-mode))
