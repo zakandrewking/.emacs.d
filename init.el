2263949b665c1e57b061800e6a9e2b1f6893e38d
Zak King <zaking17@gmail.com>
2018-01-16 15:47:57 -0800

    also disable pycompile

--- init.el
+++ init.el
@@ -94,6 +94,7 @@
   (add-hook 'python-mode-hook 'flycheck-mode)
   (add-to-list 'flycheck-disabled-checkers 'python-flake8)
   (add-to-list 'flycheck-disabled-checkers 'python-pylint)
+  (add-to-list 'flycheck-disabled-checkers 'python-pycompile)
 
   ;; web-mode
   (require 'web-mode)
